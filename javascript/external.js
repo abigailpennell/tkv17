ee209741118768c74af256b2fb2a6840edf31af9
abigailpennell <abigailetz@gmail.com>
2025-09-25 14:43:45 -0500

    Edits to code trying to fix bugs

--- javascript/external.js
+++ javascript/external.js
@@ -1,28 +1,20 @@
-// =======================
-// Task 3: User Name Prompt
-// =======================
+// username prompt
 const user_name = window.prompt("Please enter your name", "Type your name here");
 document.getElementById("user-header").innerHTML = "Hello, welcome " + user_name + "!";
 
-// =======================
-// Task 2: Output Examples
-// =======================
+// Output examples
 window.alert(5 + 6);             // Alert with calculation
 window.alert('5 + 6');           // Alert with string
 document.write('<h1>A heading</h1>');
 document.write('<p>A sentence.</p>');
 
-// =======================
-// Task 3: Variables
-// =======================
+// Variables
 const x = "web";
 const y = "mapping";
 const out = x + y;
 document.write("<p>" + out + "</p>");
 
-// =======================
-// Task 4: Operators
-// =======================
+// Operators
 let a = 22;
 const b = 33;
 document.writeln(a + b);
@@ -31,9 +23,7 @@ document.writeln(a += b);
 document.writeln("<br>");
 document.writeln(a += b);
 
-// =======================
-// Task 5: Conditional Statement
-// =======================
+// Conditional Statement
 document.writeln("<button onclick='condition()'>Conditional Test</button>");
 function condition() {
   const x = confirm("Are you sure you want to proceed?");
@@ -44,23 +34,17 @@ function condition() {
   }
 }
 
-// =======================
-// Task 6: Array
-// =======================
+// Array
 const arr = ["Mael","Eric","Randall","James"];
 for (let i = 0; i < arr.length; i++) {
   document.writeln(arr[i], "<br>");
 }
 
-// =======================
-// Homework Task: Copyright Footer
-// =======================
+// Homework Task?: Copyright Footer
 const today = new Date();
 const year = today.getFullYear();
 document.getElementById("copyright").innerHTML =
   "Copyright &copy; " + year;
 
-// =======================
-// Task 1: Example external JS test (Optional)
-// =======================
+// Task 1: Example external JS test (Optional?)
 document.write("<p>Where is my JS code?</p>");
